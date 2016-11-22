42c373146a52f27350fee564d497bad5d880ef8e
Raphael Ndwiga <raphaelndwiga@musoni.co.ke>
2016-11-22 20:36:13 +0300

    creating controller for fetching email list and rendering each of them

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,11 @@
 */
 
 Route::get('/', function () {
-  //  return view('welcome');
-    return view('layouts.app-org');
+    return view('partials.index');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+//Contacts
+Route::resource('Emails', 'EmailController');
